c6b1a5886647e12f9b4c8a13564a234e3933506f
KamilRadosz1997 <kamilradosz1997@gmail.com>
2021-01-30 12:04:05 +0100

    pp5

---
+++ src/main/java/pl/RadoszKamil/vouchershop/catalog/exceptions/ProductCatalogException.java
@@ -0,0 +1,7 @@
+package pl.RadoszKamil.vouchershop.catalog.exceptions;
+
+public class ProductCatalogException extends IllegalStateException {
+    public ProductCatalogException(String message) {
+        super(message);
+    }
+}
